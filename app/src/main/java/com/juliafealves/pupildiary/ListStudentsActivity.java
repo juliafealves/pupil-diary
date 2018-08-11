19f6b8952bbcb316eec1fc527b01fa204f35608b
juliafealves <juliafealves>
2018-08-10 23:55:30 -0300

    Created and configured project. First commit.

---
+++ app/src/main/java/com/juliafealves/pupildiary/ListStudentsActivity.java
@@ -0,0 +1,13 @@
+package com.juliafealves.pupildiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListStudentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_students);
+    }
+}
