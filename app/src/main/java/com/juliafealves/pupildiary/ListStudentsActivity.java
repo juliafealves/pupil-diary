5c03e77045bf6afdcbddfa82592329b32286c11a
juliafealves <juliafealves>
2018-08-11 02:36:16 -0300

    Created a ListView replace textviews for to list students.

--- app/src/main/java/com/juliafealves/pupildiary/ListStudentsActivity.java
+++ app/src/main/java/com/juliafealves/pupildiary/ListStudentsActivity.java
@@ -2,6 +2,8 @@ package com.juliafealves.pupildiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListStudentsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListStudentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_students);
+
+        String[] students = {"Julia", "Isabelle", "Ivana", "John"};
+        ListView lvStudents = findViewById(R.id.lvStudents);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
+        lvStudents.setAdapter(adapter);
     }
 }
