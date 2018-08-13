8e85d5cc9a9a72e8072fabf65fcfc19ecc1a30f9
juliafealves <juliafealves>
2018-08-13 19:44:54 -0300

    Added Toast for show message when clicked in the Save button.

--- app/src/main/java/com/juliafealves/pupildiary/FormActivity.java
+++ app/src/main/java/com/juliafealves/pupildiary/FormActivity.java
@@ -2,6 +2,9 @@ package com.juliafealves.pupildiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        Button save = findViewById(R.id.form_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormActivity.this, "Clicked button!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
