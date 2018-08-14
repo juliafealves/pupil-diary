14f504bb06cc2a9193f1ddd8646179a7e43ea952
juliafealves <juliafealves>
2018-08-13 22:03:45 -0300

    Created menu in action bar with menu item "save student".

--- app/src/main/java/com/juliafealves/pupildiary/FormActivity.java
+++ app/src/main/java/com/juliafealves/pupildiary/FormActivity.java
@@ -2,9 +2,13 @@ package com.juliafealves.pupildiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -12,14 +16,23 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+    }
 
-        Button save = findViewById(R.id.form_save);
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_form, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_form_save:
                 Toast.makeText(FormActivity.this, "Clicked button!", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
